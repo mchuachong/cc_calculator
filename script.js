e575536c7a7c7149d82ff4cc273d3940c97cbdc1
Mike <m.chuachong@gmail.com>
2025-01-30 22:33:24 -0500

    commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const btn1 = document.getElementById("one");
 const btn2 = document.getElementById("two");
 const btn3 = document.getElementById("three");
@@ -19,12 +21,47 @@ const btnMi = document.getElementById("minus");
 const display = document.getElementById("display");
 const preview = document.getElementById("preview");
 
-let dispArr = [1,2,3,4,5];
-let prevArr = [1,2,2,2];
+let dispArr = [];
+let prevArr = [];
 
+const eval = () => {
+    const termA = Number(prevArr.join(''));
+    const operator = isNaN(dispArr[0])?String(dispArr.shift()):dispArr[0];
+    const termB = Number(dispArr.join(''));
+    /*console.log(operator)
+    console.log(operator==='/')*/
+    prevArr=String(Number(calc(termA,termB,operator).toPrecision(12))).split('');
+    dispArr=[]
+    update()
+}
+    
 
+const calc = (a,b,o) => {
+    switch (o) {
+        case '+':
+            return a+b
+        case '-':
+            return a-b
+        case '/':
+            return a/b
+        case '*':
+            return a*b
+        default:
+            return b
+    }
+}
 
-btn0.addEventListener("click",()=>{dispArr.push(0);update()});
+    
+const update = () => {
+    display.textContent=dispArr.join("");
+    preview.textContent=prevArr.join('');
+    if(prevArr.length>13){
+        preview.textContent = `${prevArr[0]}.${prevArr.splice(1,2).join("")}E${prevArr.length+1}`;
+    }
+    }
+    
+
+btn0.addEventListener("click",()=>{!isNaN(dispArr[0])||!isNaN(dispArr[1])?dispArr.push(0):null;update()});
 btn1.addEventListener("click",()=>{dispArr.push(1);update()});
 btn2.addEventListener("click",()=>{dispArr.push(2);update()});
 btn3.addEventListener("click",()=>{dispArr.push(3);update()});
@@ -36,17 +73,29 @@ btn8.addEventListener("click",()=>{dispArr.push(8);update()});
 btn9.addEventListener("click",()=>{dispArr.push(9);update()});
 btnC.addEventListener("click",()=>{dispArr=[];prevArr=[];update()});
 btnP.addEventListener("click",()=>{dispArr.includes(".")?null:dispArr.push('.');update()});
-btnP.addEventListener("click",()=>{dispArr.includes("x")?null:dispArr.push('x');update()});
-btnP.addEventListener("click",()=>{dispArr.includes("/")?null:dispArr.push('/');update()});
-btnP.addEventListener("click",()=>{dispArr.includes("-")?null:dispArr.push('-');update()});
-btnP.addEventListener("click",()=>{dispArr.includes("+")?null:dispArr.push('+');update()});
+btnMu.addEventListener("click",()=>{prevArr.length===0?prevArr=dispArr:null;dispArr=['*'];update()});
+btnD.addEventListener("click",()=>{prevArr.length===0?prevArr=dispArr:null;dispArr=['/'];update()});
+btnA.addEventListener("click",()=>{prevArr.length===0?prevArr=dispArr:null;dispArr=['+'];update()});
+btnMi.addEventListener("click",()=>{prevArr.length===0?prevArr=dispArr:null;isNaN(dispArr[0])&&dispArr[0]!='-'?dispArr[1]='-':dispArr=['-'];update()});
+btnB.addEventListener("click",()=>{dispArr.pop();update()});
+btnE.addEventListener("click",eval);
 
 
-
-const update = () => {
-    display.textContent=dispArr.join("");
-    preview.textContent=dispArr.join('');
-    console.log(dispArr)
-}
-
-update();
+document.addEventListener("keydown",(e)=>{if(e.key==0){!isNaN(dispArr[0])||!isNaN(dispArr[1])?dispArr.push(0):null;update()}});
+document.addEventListener("keydown",(e)=>{if(e.key==1){dispArr.push(1);update()}});
+document.addEventListener("keydown",(e)=>{if(e.key==2){dispArr.push(2);update()}});
+document.addEventListener("keydown",(e)=>{if(e.key==3){dispArr.push(3);update()}});
+document.addEventListener("keydown",(e)=>{if(e.key==4){dispArr.push(4);update()}});
+document.addEventListener("keydown",(e)=>{if(e.key==5){dispArr.push(5);update()}});
+document.addEventListener("keydown",(e)=>{if(e.key==6){dispArr.push(6);update()}});
+document.addEventListener("keydown",(e)=>{if(e.key==7){dispArr.push(7);update()}});
+document.addEventListener("keydown",(e)=>{if(e.key==8){dispArr.push(8);update()}});
+document.addEventListener("keydown",(e)=>{if(e.key==9){dispArr.push(9);update()}});
+document.addEventListener("keydown",(e)=>{if(e.key=='Delete'){dispArr=[];prevArr=[];update()}});
+document.addEventListener("keydown",(e)=>{if(e.key=='.'){dispArr.includes(".")?null:dispArr.push('.');update()}});
+document.addEventListener("keydown",(e)=>{if(e.key=='*'){prevArr.length===0?prevArr=dispArr:null;dispArr=['*'];update()}});
+document.addEventListener("keydown",(e)=>{if(e.key=='/'){prevArr.length===0?prevArr=dispArr:null;dispArr=['/'];update()}});
+document.addEventListener("keydown",(e)=>{if(e.key=='+'){prevArr.length===0?prevArr=dispArr:null;dispArr=['+'];update()}});
+document.addEventListener("keydown",(e)=>{if(e.key=='-'){prevArr.length===0?prevArr=dispArr:null;isNaN(dispArr[0])&&dispArr[0]!='-'?dispArr[1]='-':dispArr=['-'];update()}});
+document.addEventListener("keydown",(e)=>{if(e.key=='Backspace'){dispArr.pop();update()}});
+document.addEventListener("keydown",(e)=>{if(e.key=='Enter'){eval()}});
